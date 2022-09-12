43cc6b8d0a48a543abb1d9ec013624688ed3b417
Yukta Varma <83545377+yukkivarma@users.noreply.github.com>
2022-09-13 00:49:31 +0530

    Update Registration.js

--- src/Registration.js
+++ src/Registration.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Login_Registration.css';
-// import Login from './Login';
+
 
 function Registration(props) {
   const navigate = useNavigate();
