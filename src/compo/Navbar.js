9be2811fd6cda6aa9b1626dbe583a273c03a7999
ronDsk <rondey84@gmail.com>
2022-09-12 20:54:35 +0530

    Update Navbar.js

--- src/compo/Navbar.js
+++ src/compo/Navbar.js
@@ -1,53 +1,53 @@
 import React from "react";
 
 import {
-    Nav,
-    NavLink,
-    Bars,
-    NavMenu,
-    NavLogo,
-  } from './NavbarElements';
+  Nav,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavLogo,
+} from './NavbarElements';
 
 const Navbar = () => {
-    return (
-        <>
-        
-           <Nav>
-            <NavLogo to="/">
-             <div className="logo"></div>
-            </NavLogo>
-            <Bars />
-            <NavMenu>
-                <NavLink 
-                  to="/"
-                  activeStyle={{ color:'black' }}
-                >
-                    Home
-                </NavLink>
-                <NavLink
-                  to="/coviddata"
-                  activeStyle={{ color: 'black' }}
-                >
-                    Country
-                </NavLink>
-                <NavLink
-                  to="/statewise"
-                  activeStyle={{ color: 'black' }}
-                >
-                    Statewise
-                </NavLink>
-                <NavLink
-                  to="/formdata"
-                  activeStyle={{ color: 'black' }}
-                >
-                    Form
-                </NavLink>
-                
-                
-                
-            </NavMenu>
-           </Nav> 
-        </>
-    );
+  return (
+    <>
+
+      <Nav>
+        <NavLogo to="/">
+          <div className="logo"></div>
+        </NavLogo>
+        <Bars />
+        <NavMenu>
+          <NavLink
+            to="/"
+            activestyle={{ color: 'black' }}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/coviddata"
+            activestyle={{ color: 'black' }}
+          >
+            Country
+          </NavLink>
+          <NavLink
+            to="/statewise"
+            activestyle={{ color: 'black' }}
+          >
+            Statewise
+          </NavLink>
+          <NavLink
+            to="/formdata"
+            activestyle={{ color: 'black' }}
+          >
+            Form
+          </NavLink>
+
+
+
+        </NavMenu>
+      </Nav>
+    </>
+  );
 };
 export default Navbar;
